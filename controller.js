cdb6d59ff87498daa9d98410043003fb82ed28c9
Tomit Huynh <tomitnh@gmail.com>
2018-03-22 14:12:33 -0400

    Update link to tutorial. Completed the print

--- controller.js
+++ controller.js
@@ -4,36 +4,51 @@
  * Controls frame animation and drawing on the canvas
  */
 
-var fps = 5; // Standard frames per second
+var fps = 15; // Standard frames per second
 window.setInterval(animate, 1000/fps);
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'dimgrey';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 var x, y, size;
 x = 0;
 y = 0;
-size = 10;
+size = 20;
 
-function draw() {
+function draw () {
 
-	ctx.fillStyle = 'green';
+	if (Math.random(1) < 0.5)
+		line(x, y, x + size, y + size);
+	else
+		line(x, y + size, x + size, y);
 
-	if (x < canvas.width) {
-		ctx.fillRect(x, y, size, size);
-		x += 15;
+	x += size;
+
+	if (x > canvas.width) {
+		x = 0;
+		y += size;
 	}
-	
 }
 
-function animate() {
+function animate () {
 
 	// clear canvas and draw new frame
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// ctx.clearRect(0, 0, canvas.width, canvas.height);
 	draw();
 }
 
-function dist(x1, y1, x2, y2) {
+function line (x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1,y1);
+	ctx.lineTo(x2,y2);
+
+	ctx.strokeStyle = 'white';
+	ctx.stroke();
+}
+
+function dist (x1, y1, x2, y2) {
 	// calculating distance with Pythagorean theorem
 	var a = x2 - x1;
 	var b = y2 - y1;
